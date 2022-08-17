477d682a2154f9303e2e4f1f4ca014172620d77b
TestUser <testuser@example.com>
2022-08-17 07:43:15 +0530

    modify region name in python code

--- ec2_list.py
+++ ec2_list.py
@@ -1,6 +1,6 @@
 # This function is written to get list of EC2 instances as Dictionary Object in Python
 import boto3
-ec2 = boto3.client('ec2',region_name="ap-south-1")
+ec2 = boto3.client('ec2',region_name="us-west-2")
 
 ec2_dict=ec2.describe_instances()
 print("ec2_dict type is",type(ec2_dict))
